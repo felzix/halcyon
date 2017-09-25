b6fa4156fc0f035f0dc4ffef133b5298580ce4de
felzix <felzix@gmail.com>
2017-09-25 16:07:38 -0700

    removed unnecessary prop

--- IndexPage.js
+++ IndexPage.js
@@ -34,7 +34,7 @@ class App extends React.Component {
 
     return (
       <div>
-        <History history={this.props.history}/>
+        <History/>
         <CommandLineInput/>
       </div>)
   }
