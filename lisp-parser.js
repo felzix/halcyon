2921b3abda73c710738263295aece180c7603b1e
felzix <felzix@gmail.com>
2017-09-30 16:12:28 -0700

    lisp parser!

--- lisp-parser.js
+++ lisp-parser.js
@@ -1,3 +1,65 @@
+import { generate } from 'pegjs'
+
+
+const grammar = `
+{
+  function makeInteger(o) {
+    return parseInt(o.join(""), 10);
+  }
+
+  function flatten(list) {
+    const helper = list => {
+      let final = []
+      for (let i = 0; i < list.length; i++) {
+        const element = list[i]
+        if (element instanceof Array) {
+          final = final.concat(helper(element))
+        } else {
+          final.push(element)
+        }
+      }
+      return final
+    }
+    return helper(list)
+  }
+
+  function removeSpaces(list) {
+    return list.filter(e => { return e !== ' ' })
+  }
+
+}
+
+start
+  = atom
+  / sexpr
+
+sexpr
+  = "(" _ ")" { return null }
+  / "(" _ "+" _ args:list  _ ")" { args = removeSpaces(flatten(args)); return flatten(args).reduce((x, y) => x + y) }
+  / "(" _ fn:symbol _ args:list  _ ")" { return eval(fn)(args[0]) }
+
+list
+  = f:expr _ r:list* { return [f, r] }
+
+expr
+  = sexpr / atom
+
+atom
+  = symbol
+  / integer
+
+symbol
+  = [a-zA-Z.+]+ [a-zA-Z.+0-9]* { return eval(text()) }
+
+integer "integer"
+  = digits:[0-9]+ { return makeInteger(digits) }
+
+_ = [ \\t\\n]*
+`
+const parser = generate(grammar)
+
+
 export default function parse(string, environment) {
-  return ''
+  if (string === '') return
+  return parser.parse(string)
 }
--- test.js
+++ test.js
@@ -13,5 +13,31 @@ test('bar', async t => {
 });
 
 test('lisp-parser :: empty string', t => {
-  t.is(parse(''), '')
+  t.is(parse(''), undefined)
+})
+
+test('lisp-parser :: null', t => {
+  t.is(parse('()'), null)
+})
+
+test('lisp-parser :: symbol', t => {
+  t.is(parse('Math.sqrt'), Math.sqrt)
+})
+
+test('lisp-parser :: addition', t => {
+  const result = parse('(+ 3 4 5)')
+  console.log(result)
+  t.is(result, 12)
+})
+
+test('lisp-parser :: square root', t => {
+  const result = parse('(Math.sqrt 4)')
+  console.log(result)
+  t.is(result, 2)
+})
+
+test('lisp-parser :: nested', t => {
+  const result = parse('(+ 5 (+ 2 7))')
+  console.log(result)
+  t.is(result, 14)
 })
