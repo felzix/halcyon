7477f782455626e79ff06d5d27f3bbb2647ad340
felzix <felzix@gmail.com>
2018-04-23 15:03:40 -0700

    moved lisp grammar to a separate file

---
+++ lisp-grammar.js
@@ -0,0 +1,81 @@
+import { generate } from "pegjs"
+
+
+const grammar = `
+{
+  function log(thing) {
+    f(thing)
+  }
+}
+
+sexpr
+  = _ d:dotty _ { return d }
+  / _ a:atom _ { return a }
+  / _ s:shorthandQuote _ { return s }
+  / _ l:list _  { return l }
+  / _ m:mapping _ { return m }
+
+atom
+  = float
+  / integer
+  / boolean
+  / nil
+  / null
+  / string
+  / symbol
+  / "." { return Symbol.for(".") }
+
+list
+  = "(" _ args:sexpr* _ ")" { return args === null ? [] : args }
+
+mapping
+  = "{" p:pair* "}" { return [Symbol.for('mapping'), [Symbol.for('list')].concat(p)] }
+
+pair
+  = _ k:sexpr _ ":" _ v:sexpr _ { return [Symbol.for('list'), k, v] }
+
+dotty
+  = f:symbol r:innerDotty+ { return [Symbol.for("."), f].concat(r) }
+
+innerDotty
+  = "." a:symbol { return a }
+  / "." a:atom { return a }
+  / "." a:shorthandQuote { return a }
+  / "." a:list { return a }
+
+shorthandQuote
+  = "'" arg:sexpr { return [Symbol.for('quote')].concat([arg]) }
+
+symbol
+  = symbolic latterSymbolic* { return Symbol.for(text()) }
+
+float
+  = "-"?[0-9]+ "." [0-9]+ { return parseFloat(text(), 10) }
+
+integer
+  = "-"?[0-9]+ { return parseInt(text(), 10) }
+
+boolean
+  = "true" { return true }
+  / "false" { return false }
+
+nil
+  = "nil" { return undefined }
+
+null
+  = "null" { return null }
+
+string
+  = '"' q:quoted* '"' { return q.join('') }
+
+quoted
+  = [^"\\\\]
+  / '\\\\"' { return '"' }
+  / '\\\\'
+
+symbolic = [a-zA-Z:;_$!?><=+*/-]
+latterSymbolic = [0-9a-zA-Z:;_$!?><=+*/-]
+_ = [ \\t\\n,]*
+`
+
+module.exports = generate(grammar)
--- lisp-parser.js
+++ lisp-parser.js
@@ -2,7 +2,7 @@
 
 import "babel-polyfill"  // necessary for await/async to work
 import $ from "jquery"
-import { generate } from "pegjs"
+
 import React from "react"
 import uuid4 from "uuid"
 import CodeMirror from "react-codemirror"  // TODO refactor away
@@ -10,84 +10,8 @@ import CodeMirror from "react-codemirror"  // TODO refactor away
 import { GeneratedElement, Editor, uploadConfig } from "./results"
 import node from "./node"
 
+import parser from "./lisp-grammar"
 
-const grammar = `
-{
-  function log(thing) {
-    f(thing)
-  }
-}
-
-sexpr
-  = _ d:dotty _ { return d }
-  / _ a:atom _ { return a }
-  / _ s:shorthandQuote _ { return s }
-  / _ l:list _  { return l }
-  / _ m:mapping _ { return m }
-
-atom
-  = float
-  / integer
-  / boolean
-  / nil
-  / null
-  / string
-  / symbol
-  / "." { return Symbol.for(".") }
-
-list
-  = "(" _ args:sexpr* _ ")" { return args === null ? [] : args }
-
-mapping
-  = "{" p:pair* "}" { return [Symbol.for('mapping'), [Symbol.for('list')].concat(p)] }
-
-pair
-  = _ k:sexpr _ ":" _ v:sexpr _ { return [Symbol.for('list'), k, v] }
-
-dotty
-  = f:symbol r:innerDotty+ { return [Symbol.for("."), f].concat(r) }
-
-innerDotty
-  = "." a:symbol { return a }
-  / "." a:atom { return a }
-  / "." a:shorthandQuote { return a }
-  / "." a:list { return a }
-
-shorthandQuote
-  = "'" arg:sexpr { return [Symbol.for('quote')].concat([arg]) }
-
-symbol
-  = symbolic latterSymbolic* { return Symbol.for(text()) }
-
-float
-  = "-"?[0-9]+ "." [0-9]+ { return parseFloat(text(), 10) }
-
-integer
-  = "-"?[0-9]+ { return parseInt(text(), 10) }
-
-boolean
-  = "true" { return true }
-  / "false" { return false }
-
-nil
-  = "nil" { return undefined }
-
-null
-  = "null" { return null }
-
-string
-  = '"' q:quoted* '"' { return q.join('') }
-
-quoted
-  = [^"\\\\]
-  / '\\\\"' { return '"' }
-  / '\\\\'
-
-symbolic = [a-zA-Z:;_$!?><=+*/-]
-latterSymbolic = [0-9a-zA-Z:;_$!?><=+*/-]
-_ = [ \\t\\n,]*
-`
-const parser = generate(grammar)
 
 // Returns a (usually nested) Array of strings
 export function parse(string) {
